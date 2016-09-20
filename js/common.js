a935ec3fad3e71ba5478404b42ecf065efaeee77
cccikov <522978139@qq.com>
2016-09-20 17:50:54 +0800

    ukrchoice

--- js/common.js
+++ js/common.js
@@ -3,3 +3,9 @@ $(function(){
 		console.clear();
 	});
 });
+function isIe8(){
+	return ccc.isXBrowser("msie 8");
+}
+function isIe9(){
+	return ccc.isXBrowser("msie 9");
+}
---
+++ js/index.js
@@ -0,0 +1,39 @@
+$(function() {
+
+    function menuShow() {
+        $("#menu").show();
+        var timer = setTimeout(function() {
+            $("#menu,.menu-c").addClass("active");
+        });
+    }
+
+    function menuHid() {
+        $("#menu,.menu-c").removeClass("active");
+        if (!isIe9() || !isIe8()) {
+            var timer = setTimeout(function() {
+                $("#menu").hide();
+            }, 250);
+        }else{
+        	 $("#menu").hide();
+        }
+
+    }
+    $("#menuBtn").on("click", function() {
+        menuShow();
+    });
+    $(".menuClose").on("click", function() {
+        menuHid();
+    });
+
+    $(window).on("keydown", function(e) {
+        console.log(e, e.keyCode);
+        e.preventDefault();
+        if (e.keyCode == 27 && $("#menu").hasClass("active")) { //Esc
+            menuHid();
+        }
+        if (e.ctrlKey && e.keyCode == 79) {
+            menuShow();
+        }
+    });
+
+});
